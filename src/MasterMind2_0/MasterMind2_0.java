bc140240a9befa6871f7f26f10c4351b60ca92ac
Ziad-024 <1210781@student.roc-nijmegen.nl>
2025-01-17 13:14:21 +0100

    kleuren function

--- src/MasterMind2_0/MasterMind2_0.java
+++ src/MasterMind2_0/MasterMind2_0.java
@@ -7,9 +7,7 @@ public class MasterMind2_0 {
 
 	public static void main(String[] args) {
 
-		Scanner sc = new Scanner(System.in);
 		int[] kleuren = { 1, 2, 3, 4, 5, 6 };
-		Random rand = new Random();
 
 		codesFunctie func = new codesFunctie();
 		int[] codes = func.codesFunctie();
@@ -29,28 +27,14 @@ public class MasterMind2_0 {
 
 			int[] antwoordcodes = new int[4];
 
-			for (int i = 0; i < antwoordcodes.length; i++) {
-				String in = sc.next();
-				int loopInput = Integer.parseInt(in);
-				if (loopInput >= 6) {
-					loopInput = 6;
-				} else if (loopInput <= 1) {
-					loopInput = 1;
-				}
-				antwoordcodes[i] = loopInput;
-			}
-
-			System.out.print("gok: ");
-			for (int a : antwoordcodes) {
-				System.out.print(a + " ");
-			}
+			
 
 			System.out.println("");
 
 			System.out.println("Hint:");
 
 			System.out.println("");
-			func.NaKijkFunction(codes, antwoordcodes);
+			func.NaKijkFunction(codes, antwoordcodes, kleuren);
 
 		} while (rij < 10);
 		System.out.println("de code was:");
--- src/MasterMind2_0/codesFunctie.java
+++ src/MasterMind2_0/codesFunctie.java
@@ -20,11 +20,29 @@ public class codesFunctie {
 
 	}
 
-	public int NaKijkFunction(int[] codes, int[] antwoordcodes) {
+	public int NaKijkFunction(int[] codes, int[] antwoordcodes, int[] kleuren) {
 		int[] hintposities = new int[4];
 		int wit = 7;
 		int zwart = 8;
 		int none = 0;
+		Scanner sc = new Scanner (System.in);
+		
+		
+		for (int i = 0; i < antwoordcodes.length; i++) {
+			String in = sc.next();
+			int loopInput = Integer.parseInt(in);
+			if (loopInput >= 6) {
+				loopInput = 6;
+			} else if (loopInput <= 1) {
+				loopInput = 1;
+			}
+			antwoordcodes[i] = loopInput;
+		}
+		
+		System.out.print("gok: ");
+		for (int a : antwoordcodes) {
+			System.out.print(a + " ");
+		}
 
 		if (antwoordcodes[0] == codes[0]) {
 			hintposities[0] = zwart;
@@ -96,4 +114,9 @@ public class codesFunctie {
 		return hintpostitiesGoed;
 
 	}
-}
+	
+	
+	
+	}
+	
+
